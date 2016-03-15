751b68ed694b3d77b0709e1813e26386e59b3444
Truong Nguyen <samuraitruong@hotmail.com>
2016-03-15 23:19:16 +1100

    add code allow generate token JWT support web api

---
+++ ComicWebApp/Controller/TokenController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using Microsoft.AspNet.Authorization;
+using System.Security.Principal;
+using Microsoft.AspNet.Authentication.JwtBearer;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ComicWebApp
+{
+    [Route("api/[controller]")]
+    public class TokenController : Controller
+    {
+        private readonly TokenAuthOptions tokenOptions;
+
+        //public TokenController(TokenAuthOptions tokenOptions)
+        public TokenController(TokenAuthOptions tokenOptions)
+        {
+            this.tokenOptions = tokenOptions;
+            //this.bearerOptions = options.Value;
+            //this.signingCredentials = signingCredentials;
+        }
+
+        /// <summary>
+        /// Check if currently authenticated. Will throw an exception of some sort which shoudl be caught by a general
+        /// exception handler and returned to the user as a 401, if not authenticated. Will return a fresh token if
+        /// the user is authenticated, which will reset the expiry.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Authorize("Bearer")]
+        public dynamic Get()
+        {
+            /* 
+            ******* WARNING WARNING WARNING ****** 
+            ******* WARNING WARNING WARNING ****** 
+            ******* WARNING WARNING WARNING ****** 
+            THIS METHOD SHOULD BE REMOVED IN PRODUCTION USE-CASES - IT ALLOWS A USER WITH 
+            A VALID TOKEN TO REMAIN LOGGED IN FOREVER, WITH NO WAY OF EVER EXPIRING THEIR
+            RIGHT TO USE THE APPLICATION.
+            Refresh Tokens (see https://auth0.com/docs/refresh-token) should be used to 
+            retrieve new tokens. 
+            ******* WARNING WARNING WARNING ****** 
+            ******* WARNING WARNING WARNING ****** 
+            ******* WARNING WARNING WARNING ****** 
+            */
+            bool authenticated = false;
+            string user = null;
+            int entityId = -1;
+            string token = null;
+            DateTime? tokenExpires = default(DateTime?);
+
+            var currentUser = HttpContext.User;
+            if (currentUser != null)
+            {
+                authenticated = currentUser.Identity.IsAuthenticated;
+                if (authenticated)
+                {
+                    user = currentUser.Identity.Name;
+                    foreach (Claim c in currentUser.Claims) if (c.Type == "EntityID") entityId = Convert.ToInt32(c.Value);
+                    tokenExpires = DateTime.UtcNow.AddMinutes(2);
+                    token = GetToken(currentUser.Identity.Name, tokenExpires);
+                }
+            }
+            return new { authenticated = authenticated, user = user, entityId = entityId, token = token, tokenExpires = tokenExpires };
+        }
+
+        public class AuthRequest
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+
+        /// <summary>
+        /// Request a new token for a given username/password pair.
+        /// </summary>
+        /// <param name="req"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public dynamic Post([FromBody] AuthRequest req)
+        {
+            // Obviously, at this point you need to validate the username and password against whatever system you wish.
+            if ((req.username == "TEST" && req.password == "TEST") || (req.username == "TEST2" && req.password == "TEST"))
+            {
+                DateTime? expires = DateTime.UtcNow.AddMinutes(2);
+                var token = GetToken(req.username, expires);
+                return new { authenticated = true, entityId = 1, token = token, tokenExpires = expires };
+            }
+            return new { authenticated = false };
+        }
+
+        private string GetToken(string user, DateTime? expires)
+        {
+            var handler = new JwtSecurityTokenHandler() {
+                TokenLifetimeInMinutes = 300
+            };
+
+            // Here, you should create or look up an identity for the user which is being authenticated.
+            // For now, just creating a simple generic identity.
+            ClaimsIdentity identity = new ClaimsIdentity(new GenericIdentity(user, "TokenAuth"), new[] { new Claim("EntityID", "1", ClaimValueTypes.Integer) });
+
+            var securityToken = handler.CreateToken(
+                issuer: tokenOptions.Issuer,
+                audience: tokenOptions.Audience,
+                signingCredentials: tokenOptions.SigningCredentials,
+                subject: identity,
+                expires: expires
+                );
+            return handler.WriteToken(securityToken);
+        }
+    }
+}
--- ComicWebApp/Startup.cs
+++ ComicWebApp/Startup.cs
@@ -9,15 +9,59 @@ using Microsoft.Extensions.DependencyInjection;
 using ComicWeb.Core;
 using ComicWeb.JsonService;
 using System.IO;
+using System.IdentityModel.Tokens;
+using Microsoft.AspNet.Authorization;
+using Microsoft.AspNet.Authentication.JwtBearer;
+using System.Security.Cryptography;
+using Microsoft.AspNet.Diagnostics;
+using Newtonsoft.Json;
 
 namespace ComicWebApp
 {
     public class Startup
     {
+        const string TokenAudience = "ExampleAudience";
+        const string TokenIssuer = "ExampleIssuer";
+        private RsaSecurityKey key;
+        private TokenAuthOptions tokenOptions;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // *** CHANGE THIS FOR PRODUCTION USE ***
+            // Here, we're generating a random key to sign tokens - obviously this means
+            // that each time the app is started the key will change, and multiple servers 
+            // all have different keys. This should be changed to load a key from a file 
+            // securely delivered to your application, controlled by configuration.
+            //
+            // See the RSAKeyUtils.GetKeyParameters method for an examle of loading from
+            // a JSON file.
+            RSAParameters keyParams = RSAKeyUtils.GetRandomKey();
+
+            // Create the key, and a set of token options to record signing credentials 
+            // using that key, along with the other parameters we will need in the 
+            // token controlller.
+            key = new RsaSecurityKey(keyParams);
+            tokenOptions = new TokenAuthOptions()
+            {
+                Audience = TokenAudience,
+                Issuer = TokenIssuer,
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.RsaSha256Signature)
+            };
+
+            // Save the token options into an instance so they're accessible to the 
+            // controller.
+            services.AddInstance<TokenAuthOptions>(tokenOptions);
+
+            // Enable the use of an [Authorize("Bearer")] attribute on methods and classes to protect.
+            services.AddAuthorization(auth =>
+            {
+                auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme‌​)
+                    .RequireAuthenticatedUser().Build());
+            });
+
             //services.AddApplicationInsightsTelemetry(Configuration);
             services.AddMvc();
 
@@ -35,6 +79,60 @@ namespace ComicWebApp
             var realPath = hostingEnvironment.WebRootPath;// + ctx.Request.Path.Value;
 
             service.SetDataFolder(realPath +"\\app_data");
+
+            app.UseExceptionHandler(appBuilder =>
+            {
+                appBuilder.Use(async (context, next) =>
+                {
+                    var error = context.Features[typeof(IExceptionHandlerFeature)] as IExceptionHandlerFeature;
+                    // This should be much more intelligent - at the moment only expired 
+                    // security tokens are caught - might be worth checking other possible 
+                    // exceptions such as an invalid signature.
+                    if (error != null && error.Error is SecurityTokenExpiredException)
+                    {
+                        context.Response.StatusCode = 401;
+                        // What you choose to return here is up to you, in this case a simple 
+                        // bit of JSON to say you're no longer authenticated.
+                        context.Response.ContentType = "application/json";
+                        await context.Response.WriteAsync(
+                            JsonConvert.SerializeObject(
+                                new { authenticated = false, tokenExpired = true }));
+                    }
+                    else if (error != null && error.Error != null)
+                    {
+                        context.Response.StatusCode = 500;
+                        context.Response.ContentType = "application/json";
+                        // TODO: Shouldn't pass the exception message straight out, change this.
+                        await context.Response.WriteAsync(
+                            JsonConvert.SerializeObject
+                            (new { success = false, error = error.Error.Message }));
+                    }
+                    // We're not trying to handle anything else so just let the default 
+                    // handler handle.
+                    else await next();
+                });
+            });
+
+            app.UseJwtBearerAuthentication(options =>
+            {
+                // Basic settings - signing key to validate with, audience and issuer.
+                options.TokenValidationParameters.IssuerSigningKey = key;
+                options.TokenValidationParameters.ValidAudience = tokenOptions.Audience;
+                options.TokenValidationParameters.ValidIssuer = tokenOptions.Issuer;
+
+                // When receiving a token, check that we've signed it.
+                options.TokenValidationParameters.ValidateSignature = true;
+
+                // When receiving a token, check that it is still valid.
+                options.TokenValidationParameters.ValidateLifetime = true;
+
+                // This defines the maximum allowable clock skew - i.e. provides a tolerance on the token expiry time 
+                // when validating the lifetime. As we're creating the tokens locally and validating them on the same 
+                // machines which should have synchronised time, this can be set to zero. Where external tokens are
+                // used, some leeway here could be useful.
+                options.TokenValidationParameters.ClockSkew = TimeSpan.FromMinutes(0);
+            });
+
             //app.Use(async (ctx, next) =>
             //{
             //    var hostingEnvironment = app.ApplicationServices.GetService<IHostingEnvironment>();
