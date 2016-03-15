b91cc3f8231f94106cc50eabba2022637ad76da0
Truong Nguyen <samuraitruong@hotmail.com>
2016-03-15 23:24:16 +1100

    just new example....

--- ComicWebApp/Controller/TokenController.cs
+++ ComicWebApp/Controller/TokenController.cs
@@ -23,12 +23,39 @@ namespace ComicWebApp
             //this.signingCredentials = signingCredentials;
         }
 
+        [HttpGet("test")]
+        //[Authorize("Bearer")]
+        public dynamic Get1()
+        {
+            bool authenticated = false;
+            string user = null;
+            int entityId = -1;
+            string token = null;
+            DateTime? tokenExpires = default(DateTime?);
+
+
+            var currentUser = HttpContext.User;
+            if (currentUser != null)
+            {
+                authenticated = currentUser.Identity.IsAuthenticated;
+                if (authenticated)
+                {
+                    user = currentUser.Identity.Name;
+                    foreach (Claim c in currentUser.Claims) if (c.Type == "EntityID") entityId = Convert.ToInt32(c.Value);
+                    tokenExpires = DateTime.UtcNow.AddMinutes(2);
+                    token = GetToken(currentUser.Identity.Name, tokenExpires);
+                }
+            }
+            return new { authenticated = authenticated, user = user, entityId = entityId, token = token, tokenExpires = tokenExpires };
+
+        }
         /// <summary>
         /// Check if currently authenticated. Will throw an exception of some sort which shoudl be caught by a general
         /// exception handler and returned to the user as a 401, if not authenticated. Will return a fresh token if
         /// the user is authenticated, which will reset the expiry.
         /// </summary>
         /// <returns></returns>
+        /// 
         [HttpGet]
         [Authorize("Bearer")]
         public dynamic Get()
